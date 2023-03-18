f5cdfdac977b70d57459a2c59078af28f72c468a
DilayGulhan <dilaygulhan@gmail.com>
2023-03-18 13:51:33 +0300

    @Named

---
+++ src/main/java/com/Dilay/cdi/NamedTut.java
@@ -0,0 +1,21 @@
+package com.Dilay.cdi;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
+import lombok.*;
+import lombok.extern.log4j.Log4j;
+import jakarta.inject.Named;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+//cdi ==> bean
+@Named(value = "namedTutor")
+@ApplicationScoped
+public class NamedTut {
+    private Long namedId ;
+    private String namedata  = "Merhabalar Named Cdi Bean";
+
+}
